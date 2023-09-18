6f63ab61fd6ec3d7b2ef12ef47bd2ca04dff1df5
victorJss <volts378@gmail.com>
2023-09-17 22:08:13 -0300

    verificações do otp

--- js/otp.js
+++ js/otp.js
@@ -1,6 +1,8 @@
 const slotsInputs = document.querySelectorAll(".conteudo__sessao-principal__codigo__slot");
 const botaoProximo = document.querySelector(".conteudo__sessao-principal__botoes-voltar-proximo__proximo");
 
+     // verificação para saber se é o último input e para dar destaque no botão proximo
+
 slotsInputs.forEach( (slot) => {
 
     slot.addEventListener("keyup", () => {
@@ -8,10 +10,10 @@ slotsInputs.forEach( (slot) => {
     
         if (tamanhoSlot > 0) {
             const proximoInput = slot.nextElementSibling;
+
             if(proximoInput === null){
                 slot.blur() 
-              botaoProximo.style.opacity = "100%"
-              
+                botaoProximo.style.opacity = "100%"
             }else{
                 proximoInput.focus()
             }
@@ -20,31 +22,35 @@ slotsInputs.forEach( (slot) => {
         if(tamanhoSlot === 0){
             botaoProximo.style.opacity = "49%"
         }
-
     });
 });
 
+// verificação para saber se os valores apresentados pelo usuário está correto!
+
 botaoProximo.addEventListener('click', () => {
     
-    const slotFilter = [];
+    const slotVazio = [];
+    const slotDoisDigitos = [];
+
+    let verificaSlotVazio 
+    let verificaQuantidadeSlot
 
     slotsInputs.forEach((slots) => {
-        if(slots.value.length === 0){
-            slotFilter.push(slots)
-        }
+        
+        verificaSlotVazio = slots.value.length === 0? slotVazio.push(slots) : false;  
+        verificaQuantidadeSlot = slots.value.length > 1?   slotDoisDigitos.push(slots) : false;
+        
     })
-
-    if(slotFilter.length > 0){
-        alert("Digite os 4 digitos corretamente!");
-    }else{
+  
+    if(slotVazio.length === 0 && slotDoisDigitos.length === 0){
         window.location.href = "./home.html"
+    }else{
+        alert("Código inválido!"); 
     }
-
-
 })
-
 
 function voltarLogin(){
     window.location.href ="./login.html"
+    
 }
 
